6e46423c5ca7e5debe7c2577e7fcd3f1c13f443b
tamaarory <tamaarory@gmail.com>
2021-06-20 22:35:50 +0700

    tambah module Jenis Surat (Api CRUD dan table migration)

---
+++ api/database/migrations/2021_06_16_085044_create_jenis_surat_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJenisSuratTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jenis_surat', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_surat', 10);
+            $table->string('nama_jenis_surat', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jenis_surat');
+    }
+}
